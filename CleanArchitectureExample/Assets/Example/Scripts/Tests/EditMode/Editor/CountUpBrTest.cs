b5eef9a929e1f3a38c9b5ae81ed30b92e76e1de0
nakano yosuke <naninuneno0411@gmail.com>
2019-12-05 01:46:30 +0900

    add test (ValueObject/BusinessRule)

---
+++ CleanArchitectureExample/Assets/Example/Scripts/Tests/EditMode/Editor/CountUpBrTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Example.Domain;
+
+namespace Example.Tests.EditMode {
+    public class CountUpBrTest {
+
+        CountUpBr countUp;
+        CountVo count;
+
+        [SetUp]
+        public void SetUpTest() {
+            countUp = new CountUpBr(10);
+            count = new CountVo(0);
+        }
+
+        [Test]
+        public void CountUpTest() {
+            Assert.AreEqual(1, countUp.CountUp(count));
+            Assert.AreEqual(2, countUp.CountUp(count));
+            Assert.AreEqual(3, countUp.CountUp(count));
+            Assert.AreEqual(4, countUp.CountUp(count));
+            Assert.AreEqual(5, countUp.CountUp(count));
+            Assert.AreEqual(6, countUp.CountUp(count));
+            Assert.AreEqual(7, countUp.CountUp(count));
+            Assert.AreEqual(8, countUp.CountUp(count));
+            Assert.AreEqual(9, countUp.CountUp(count));
+            Assert.AreEqual(10, countUp.CountUp(count));
+            Assert.AreEqual(10, countUp.CountUp(count));
+        }
+    }
+}
---
+++ CleanArchitectureExample/Assets/Example/Scripts/Tests/EditMode/Editor/CountVoTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Example.Tests.EditMode {
+    public class CountVoTest {
+        CountVo count;
+
+        [Test]
+        public void CountTest() {
+            count = new CountVo(100);
+            Assert.AreEqual(100, count.Count);
+            count.Add();
+            Assert.AreEqual(101, count.Count);
+        }
+    }
+}

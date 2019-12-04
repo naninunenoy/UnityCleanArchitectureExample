12f70ba242f12ed553c38350463c2504b00120bc
nakano yosuke <naninuneno0411@gmail.com>
2019-12-05 01:29:06 +0900

    testのasmdef

---
+++ CleanArchitectureExample/Assets/Example/Scripts/Tests/PlayMode/NewTestScriptPlayMode.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+namespace Example.Tests.EditMode {
+    public class NewTestScriptPlayMode {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptPlayModeSimplePasses() {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptPlayModeWithEnumeratorPasses() {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
